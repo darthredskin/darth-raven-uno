238103c7dffc18a00f7f81664b326f1304c6bd9f
Jared Bardell <darthredskin@users.noreply.github.com>
2015-01-22 03:01:39 -0500

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -5,6 +5,11 @@ public class Introductions {
     public static void main(String[] args) {
         Console console = System.console();
         // Welcome to the Introductions program!  Your code goes below here
-        console.printf("Hello, my name is Jared");
+        String firstName = console.readLine("What is your first name?"  );
+        String lastName = console.readLine("What is your last name?"  );
+      //This is an example of camelCasing
+        console.printf("Hello, my name is %s\n", firstName);
+        console.printf("My last name is %s/n", lastName);
+        console.printf("%s and I am learning how to write Java\n", firstName);
   }
 }
